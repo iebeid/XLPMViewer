da31be006310201de19e16a511cf6a0faa1b7207
Islam Akef Ebeid <iaebeid@ualr.edu>
2015-05-11 14:37:18 -0500

    Fixed third level Added Third Level to Second level view Fixed the click problem Added some PDB file upload ability

--- js/inputs.js
+++ js/inputs.js
@@ -1,15 +1,19 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Author : Islam Akef Ebeid
+ Affiliations : University of Arkansas at Little Rock - Emerging Analytics Center
  */
 
 
 var fastaText = "";
 var dataText = "";
+var firstPDB = "";
+var secondPDB = "";
+localFlag = false;
+$(document).ready(function () {
 
 
-window.onload = function () {
+    $("#localDiv").hide();
+    $("#serverDiv").hide();
     $("#vis").hide();
     $("#structure").hide();
     var fastafileInput = document.getElementById('fastafile');
@@ -40,48 +44,119 @@ window.onload = function () {
             alert("File not supported!");
         }
     });
-};
-                 
 
-function view(query,id) {
-    //pdb:4F5S','pdb:4F5S'
-    console.log(query);
-    var query = "pdb:"+query;
-    var glmol01 = new GLmol(id, true);
-   var baseURL = '';
-   if (query.substr(0, 4) == 'pdb:') {
-      query = query.substr(4).toUpperCase();
-      if (!query.match(/^[1-9][A-Za-z0-9]{3}$/)) {
-         alert("Wrong PDB ID"); return;
-      }
-      uri = "http://www.pdb.org/pdb/files/" + query + ".pdb";
-   } else if (query.substr(0, 4) == 'cid:') {
-      query = query.substr(4);
-      if (!query.match(/^[1-9]+$/)) {
-         alert("Wrong Compound ID"); return;
-      }
-      uri = "http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/" + query + 
-        "/SDF?record_type=3d";
-   }
-   glmol01.defineRepresentation = function() {
-   var all = this.getAllAtoms();
-   var hetatm = this.removeSolvents(this.getHetatms(all));
-   this.colorByAtom(all, {});
-   this.colorByChain(all);
-   var asu = new THREE.Object3D();
-   this.drawBondsAsStick(asu, hetatm, this.cylinderRadius, this.cylinderRadius);
-   this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['A'])), [58, 87]), this.cylinderRadius, this.cylinderRadius);
-   this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['B'])), [63, 92]), this.cylinderRadius, this.cylinderRadius);
-   this.drawCartoon(asu, all, this.curveWidth, this.thickness);
-   this.drawSymmetryMates2(this.modelGroup, asu, this.protein.biomtMatrices);
-   this.modelGroup.add(asu);
-};
-   $.get(uri, function(ret) {
-       $("#" + id + "_src").val(ret);
-      glmol01.loadMolecule();
-   });
+    $("input[type='radio']").click(function () {
+        if ($(this).attr("id") == "local") {
+            $("#localDiv").show();
+            $("#serverDiv").hide();
+            localFlag = true;
+        }
+
+        if ($(this).attr("id") == "server") {
+            $("#serverDiv").show();
+            $("#localDiv").hide();
+            localFlag = false;
+        }
+    });
+
+    var firstPdbInput = document.getElementById('viewMol1');
+    firstPdbInput.addEventListener('change', function (e) {
+        var file = firstPdbInput.files[0];
+        var textType = /pdb.*/;
+        if (file.name.match(textType)) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                firstPDB = reader.result;
+            };
+            reader.readAsText(file);
+        } else {
+            alert("File not supported!");
+        }
+    });
+
+    var secondPdbInput = document.getElementById('viewMol2');
+    secondPdbInput.addEventListener('change', function (e) {
+        var file = secondPdbInput.files[0];
+        var textType = /pdb.*/;
+        if (file.name.match(textType)) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                secondPDB = reader.result;
+            };
+            reader.readAsText(file);
+        } else {
+            alert("File not supported!");
+        }
+    });
+
+
+});
+
+function handleFirstPDB(id) {
+    var glmol01Obj = new GLmol(id, true);
+    glmol01Obj.defineRepresentation = function () {
+        var all = this.getAllAtoms();
+        var hetatm = this.removeSolvents(this.getHetatms(all));
+        this.colorByAtom(all, {});
+        this.colorByChain(all);
+        var asu = new THREE.Object3D();
+        this.drawBondsAsStick(asu, hetatm, this.cylinderRadius, this.cylinderRadius);
+        this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['A'])), [58, 87]), this.cylinderRadius, this.cylinderRadius);
+        this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['B'])), [63, 92]), this.cylinderRadius, this.cylinderRadius);
+        this.drawCartoon(asu, all, this.curveWidth, this.thickness);
+        this.drawSymmetryMates2(this.modelGroup, asu, this.protein.biomtMatrices);
+        this.modelGroup.add(asu);
+    };
+    $("#" + id + "_src").val(firstPDB);
+
+    glmol01Obj.loadMolecule();
 }
 
-function enableView(){
-    $("#structure").show();
+function handleSecondPDB(id) {
+    var glmol01Obj = new GLmol(id, true);
+    glmol01Obj.defineRepresentation = function () {
+        var all = this.getAllAtoms();
+        var hetatm = this.removeSolvents(this.getHetatms(all));
+        this.colorByAtom(all, {});
+        this.colorByChain(all);
+        var asu = new THREE.Object3D();
+        this.drawBondsAsStick(asu, hetatm, this.cylinderRadius, this.cylinderRadius);
+        this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['A'])), [58, 87]), this.cylinderRadius, this.cylinderRadius);
+        this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['B'])), [63, 92]), this.cylinderRadius, this.cylinderRadius);
+        this.drawCartoon(asu, all, this.curveWidth, this.thickness);
+        this.drawSymmetryMates2(this.modelGroup, asu, this.protein.biomtMatrices);
+        this.modelGroup.add(asu);
+    };
+    $("#" + id + "_src").val(secondPDB);
+    glmol01Obj.loadMolecule();
+}
+
+function view(query, id) {
+
+    var glmol01Obj = new GLmol(id, true);
+    var uri = '';
+    query = query.toUpperCase();
+
+    if (!query.match(/^[1-9][A-Za-z0-9]{3}$/)) {
+        alert("Wrong PDB ID");
+        return;
+    }
+    uri = "http://www.rcsb.org/pdb/files/" + query + ".pdb";
+    glmol01Obj.defineRepresentation = function () {
+        var all = this.getAllAtoms();
+        var hetatm = this.removeSolvents(this.getHetatms(all));
+        this.colorByAtom(all, {});
+        this.colorByChain(all);
+        var asu = new THREE.Object3D();
+        this.drawBondsAsStick(asu, hetatm, this.cylinderRadius, this.cylinderRadius);
+        this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['A'])), [58, 87]), this.cylinderRadius, this.cylinderRadius);
+        this.drawBondsAsStick(asu, this.getResiduesById(this.getSidechains(this.getChain(all, ['B'])), [63, 92]), this.cylinderRadius, this.cylinderRadius);
+        this.drawCartoon(asu, all, this.curveWidth, this.thickness);
+        this.drawSymmetryMates2(this.modelGroup, asu, this.protein.biomtMatrices);
+        this.modelGroup.add(asu);
+    };
+    $.get(uri, function (ret) {
+        $("#" + id + "_src").val(ret);
+        glmol01Obj.loadMolecule();
+    });
 }
